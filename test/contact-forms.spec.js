08df47cfb1dae6e8d37d22b14f65beb45ce7dd1c
Kenn Sippell <kennsippell@gmail.com>
2020-04-27 17:50:55 -0700

    Publish as 30-beta.0

---
+++ test/contact-forms.spec.js
@@ -0,0 +1,101 @@
+const chai = require('chai');
+const moment = require('moment');
+const chaiExclude = require('chai-exclude');
+const path = require('path');
+const Harness = require('../src/harness');
+
+chai.use(chaiExclude);
+const { expect } = chai;
+
+
+const harness = new Harness({
+  directory: path.join(__dirname, 'collateral'),
+  xformFolderPath: path.join(__dirname, 'collateral', 'forms'),
+  verbose: false,
+  reportFormErrors: false,
+});
+
+describe('contact forms', () => {
+  before(async () => { return await harness.start(); });
+  after(async () => { return await harness.stop(); });
+  beforeEach(async () => { return await harness.clear(); });
+  afterEach(() => { expect(harness.consoleErrors).to.be.empty; });
+
+  it('district-hospital with new primary contact', async () => {
+    const now = moment('2000-01-01');
+    await harness.setNow(now.valueOf());
+    const result = await harness.fillContactForm(
+      'district_hospital',
+      ['new_person', 'Full', 'Short', '1990-08-06', undefined, '+1-555-227-7744', undefined, 'female', 'patient'],
+      ['yes']
+    );
+    expect(result.errors).to.be.empty;
+    expect(result.contacts).excluding(['_id', 'meta']).to.deep.eq([
+      {
+        contact: {
+          _id: result.contacts[1]._id,
+        },
+        external_id: '',
+        geolocation: '',
+        is_name_generated: 'true',
+        name: "'s Health Facility", // actual bug in form
+        notes: '',
+        parent: {},
+        reported_date: now.valueOf(),
+        type: 'district_hospital',
+      },
+      {
+        date_of_birth: '1990-08-06',
+        date_of_birth_method: '',
+        ephemeral_dob: {
+          dob_approx: '2000-01-01',
+          dob_calendar: '1990-08-06',
+          dob_iso: '1990-08-06',
+          dob_method: '',
+          dob_raw: '1990-08-06',
+          ephemeral_months: '1',
+          ephemeral_years: '2000',
+        },
+        external_id: '',
+        name: 'Full',
+        notes: '',
+        parent: {
+          _id: result.contacts[0]._id,
+          parent: {}
+        },
+        phone: '+1-555-227-7744',
+        phone_alternate: '',
+        reported_date: now.valueOf(),
+        role: 'patient',
+        sex: 'female',
+        short_name: 'Short',
+        type: 'person',
+      },
+    ]);
+  });
+
+  it('msf-niger investigator', async () => {
+    const result = await harness.fillContactForm('investigator', ['Mr. Investigator']);
+    expect(result.errors).to.be.empty;
+
+    expect(result.contacts.length).to.eq(2);
+    expect(result.contacts[0]).to.deep.include({
+      contact: {
+        _id: result.contacts[1]._id,
+      },
+      name: 'Mr. Investigator',
+      type: 'contact',
+      contact_type: 'investigator',
+    });
+
+    expect(result.contacts[1]).to.deep.include({
+      name: 'Mr. Investigator',
+      type: 'contact',
+      parent: {
+        _id: result.contacts[0]._id,
+        parent: {},
+      },
+      contact_type: 'person',
+    });
+  });
+});
