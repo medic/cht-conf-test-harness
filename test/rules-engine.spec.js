5ad82d82c9a86f1b8eb9bbe746b8cb8e1607fa80
Kenn Sippell <kennsippell@gmail.com>
2021-05-14 13:51:15 -0700

    Tests for countSummary

--- test/rules-engine.spec.js
+++ test/rules-engine.spec.js
@@ -3,6 +3,7 @@ const { expect } = require('chai');
 const Harness = require('../src/harness');
 
 const { availableCoreVersions } = require('../src/cht-core-libs');
+// const availableCoreVersions = ['3.11'];
 
 for (const coreVersion of availableCoreVersions) {
   describe(`tests targeting rules engine v${coreVersion}`, () => {
@@ -42,6 +43,15 @@ for (const coreVersion of availableCoreVersions) {
           'emission.icon': 'newborn',
           'emission.actions[0].form': 'pnc_followup',
         });
+
+        const stateCount = await harness.countTaskDocsByState({ freshTaskDocs: false });
+        expect(stateCount).to.deep.eq({
+          Cancelled: 0,
+          Completed: 0,
+          Draft: 0,
+          Failed: 0,
+          Ready: 1,
+        });
       });
     
       it('followup task present three days after schedule', async () => {
@@ -52,6 +62,16 @@ for (const coreVersion of availableCoreVersions) {
         const tasks = await harness.getTasks();
         expect(tasks).to.have.property('length', 1);
         expect(tasks[0]).to.nested.include({ state: 'Ready' });
+
+        await harness.flush(30);
+        const stateCount = await harness.countTaskDocsByState();
+        expect(stateCount).to.deep.eq({
+          Cancelled: 0,
+          Completed: 0,
+          Draft: 0,
+          Failed: 1,
+          Ready: 0,
+        });
       });
     
       it('followup task not present at time of scheduling', async () => {
@@ -61,11 +81,39 @@ for (const coreVersion of availableCoreVersions) {
       });
     });
 
-    describe('loadAction', () => {
-      
-      beforeEach(async () => await harness.setNow('2000-01-01'));
+    describe('getTaret', () => {
+      it('basic trigger', async () => {
+        const supervisorContact = {
+          _id: 'super_id',
+          type: 'health_center',
+          reported_date: 1,
+        };
+        harness.state.contacts = [supervisorContact];
+    
+        harness.pushMockedDoc({ form: 'supervision_with_chw_confirmation', patient_id: 'super_id', reported_date: Date.now() });
+        harness.pushMockedDoc({ form: 'supervision_without_chw_confirmation', patient_id: 'super_id', reported_date: Date.now() });
+        harness.pushMockedDoc({ form: 'individual_feedback_confirmation', patient_id: 'super_id', reported_date: Date.now() });
+        
+        const targets = await harness.getTargets({ type: 'chv-receive-supervision-visit' });
+        expect(targets).to.have.property('length', 1);
+        expect(targets[0]).to.nested.include({ 'value.total': 1, 'value.pass': 1 });
+      });
+    
+      it('reports outside of current month are not considered', async () => {
+        harness.pushMockedDoc({ form: 'supervision_with_chw_confirmation', patient_id: 'parent_id_data', reported_date: 1000000000000 });
+        harness.pushMockedDoc({ form: 'supervision_without_chw_confirmation', patient_id: 'parent_id_data' });
+        harness.pushMockedDoc({ form: 'individual_feedback_confirmation', patient_id: 'parent_id_data' });
+        
+        const targets = await harness.getTargets({ type: 'chv-receive-supervision-visit' });
+        expect(targets).to.have.property('length', 1);
+        expect(targets[0]).to.nested.include({ 'value.total': 1, 'value.pass': 0 });
+      });
+    });
 
+    describe('loadAction', () => {
       it('receives action directly + multi-stage fill', async () => {
+        await harness.setNow('2000-01-01');
+
         const scheduledDate = '2000-01-07';
         const initialResult = await harness.fillForm('pnc_followup', ['no'], ['yes', scheduledDate]);
         expect(initialResult.errors).to.be.empty;
@@ -90,6 +138,8 @@ for (const coreVersion of availableCoreVersions) {
       });
 
       it('receives task document + single-stage fill', async () => {
+        await harness.setNow('2000-01-01');
+
         const scheduledDate = '2000-01-07';
         const initialResult = await harness.fillForm('pnc_followup', ['no'], ['yes', scheduledDate]);
         expect(initialResult.errors).to.be.empty;
