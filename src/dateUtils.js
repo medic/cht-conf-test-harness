7393bff7eec71991be98a48034f1f6f2e85a398d
Elijah Gichinga <elijah@medicmobile.org>
2021-06-22 12:03:22 +0300

    Object type

--- src/dateUtils.js
+++ src/dateUtils.js
@@ -5,8 +5,6 @@ const toDate = val => {
     return val;
   } else if (typeof val === 'number'){
     return DateTime.fromMillis(val);
-  } else if (typeof val === 'object' && typeof val.getMonth === 'function'){
-    return DateTime.fromJSDate(val);
   } else if (typeof val === 'string'){
     if (val.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)){
       return DateTime.fromISO(val);
@@ -15,6 +13,9 @@ const toDate = val => {
     }
     return DateTime.fromRFC2822(val);
   } else if (typeof val === 'object'){
+    if (typeof val.getMonth === 'function'){
+      return DateTime.fromJSDate(val);
+    }
     return DateTime.fromObject(val);
   }
   throw 'Unsupported date value';
