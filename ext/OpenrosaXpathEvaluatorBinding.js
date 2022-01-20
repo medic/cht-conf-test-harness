4c4d651b700463edea7b098df192fc8f7270cb46
Kenn <kennsippell@gmail.com>
2022-01-20 13:16:25 +0300

    Call medicExtensions.init

--- ext/OpenrosaXpathEvaluatorBinding.js
+++ ext/OpenrosaXpathEvaluatorBinding.js
@@ -1,6 +1,8 @@
 var _ = require('underscore'),
     ExtendedXpathEvaluator = require('extended-xpath'),
     openrosaExtensions = require('openrosa-xpath-extensions'),
+    moment = require('moment'),
+    { toBik_text } = require('bikram-sambat'),
 
     /* This file changed from v3.6 > v3.7, but not v3.7 > v3.11 */
     medicExtensions = require('cht-core-3-14/webapp/src/js/enketo/medic-xpath-extensions'),
@@ -23,6 +25,9 @@ module.exports = function() {
         extensions._now = _now;
         medicExtensions.func.now =
           medicExtensions.func.today = function() { return { t: 'date', v: _now() }; };
+
+        const zscoreUtil = {};
+        medicExtensions.init(zscoreUtil, toBik_text, moment);
         extensions.func = _.extend(extensions.func, medicExtensions.func);
         var wrappedXpathEvaluator = function(v) {
             // Node requests (i.e. result types greater than 3 (BOOLEAN)
