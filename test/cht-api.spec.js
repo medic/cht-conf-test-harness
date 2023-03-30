6ddfd1b7fff7bf13e32c771d38826db562806928
ernestoteo <foromo@medic.org>
2023-03-30 18:22:34 +0000

    Fix cht.v1 in contact summary unit test

--- test/cht-api.spec.js
+++ test/cht-api.spec.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const Harness = require('../src/harness');
 
-describe('initializations', () => {
+describe('test cht.v1 in contact summary ', () => {
 
   const harness = new Harness({
     directory: path.join(__dirname, 'collateral', 'project-with-source'),
@@ -16,7 +16,16 @@ describe('initializations', () => {
   beforeEach(async () => { return await harness.clear(); });
   afterEach(() => { expect(harness.consoleErrors).to.be.empty; });
 
-  it('cht API in contact summary', async () => {
+  it('cht API in contact summary - user has permissions', async () => {   
+    const contactSummary = await harness.getContactSummary();
+    expect(contactSummary).to.not.be.empty;
+    expect(contactSummary.context).to.not.be.undefined;
+    expect(contactSummary.context.hasPermissions).to.be.true;
+    expect(contactSummary.context.hasAnyPermission).to.be.true;
+  });
+
+  it('cht API in contact summary - user has no permissions ', async () => {
+    harness.userRoles = ['other'];
     const contactSummary = await harness.getContactSummary();
     expect(contactSummary).to.not.be.empty;
     expect(contactSummary.context).to.not.be.undefined;
--- test/collateral/project-with-source/contact-summary.js
+++ test/collateral/project-with-source/contact-summary.js
@@ -1,9 +1,6 @@
-const hasPermissions = cht.v1.hasPermissions(['can_view_analytics_tab', 'can_verify_reports']);
-const hasAnyPermission = cht.v1.hasPermissions(['can_view_analytics_tab', 'can_verify_reports']);
-
 const context = {
-  hasPermissions: hasPermissions,
-  hasAnyPermission:hasAnyPermission,
+  hasPermissions: cht.v1.hasPermissions('chw_permission'),
+  hasAnyPermission:cht.v1.hasAnyPermission([['chw_permission']])
 };
 
 module.exports = {
