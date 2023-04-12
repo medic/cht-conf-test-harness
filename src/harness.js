33d3e8959ce5614fbf9abf67b268f37aa32cd6c1
Kenn Sippell <kennsippell@gmail.com>
2023-04-12 13:09:14 -0700

    Workaround for ERR_REQUEST_RANGE_NOT_SATISFIABLE

--- src/harness.js
+++ src/harness.js
@@ -514,11 +514,12 @@ class Harness {
   get consoleErrors() {
     return this._state.console
       .filter(msg => msg.type() !== 'log')
-      .filter(msg => msg.text() !== 'Failed to load resource: net::ERR_UNKNOWN_URL_SCHEME')
-      .filter(msg => msg.text() !== 'Failed to load resource: net::ERR_FILE_NOT_FOUND') // BUG
-      .filter(msg => !msg.text().startsWith('Error fetching media file')) // BUG
-      .filter(msg => !msg.text().startsWith('Deprecation warning:')) // BUG
-      .filter(msg => !msg.text().includes('with null-based index')) // BUG
+      .filter(msg => msg.text() !== 'Failed to load resource: net::ERR_REQUEST_RANGE_NOT_SATISFIABLE') // BUG: #219
+      .filter(msg => msg.text() !== 'Failed to load resource: net::ERR_UNKNOWN_URL_SCHEME') // BUG: #220
+      .filter(msg => msg.text() !== 'Failed to load resource: net::ERR_FILE_NOT_FOUND') // BUG: #221
+      .filter(msg => !msg.text().startsWith('Error fetching media file')) // BUG: #222
+      .filter(msg => !msg.text().startsWith('Deprecation warning:')) // BUG: #223
+      .filter(msg => !msg.text().includes('with null-based index')) // BUG: #224
       .filter(msg => msg.text() !== 'Data node: /*/meta/deprecatedID with null-based index: undefined not found. Ignored.') // BUG
     ;
 
