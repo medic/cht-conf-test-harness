3bda9f4765a71fd57482597629ed4385bc389880
Kenn Sippell <kennsippell@gmail.com>
2023-04-19 12:49:29 -0700

    Add missing jQuery boilerplate from cht-core to handle self-closing tags in model.xml (#228)

--- src/form-host/cht-environment.js
+++ src/form-host/cht-environment.js
@@ -22,5 +22,14 @@ window.restoreTimers = () => clock && clock.uninstall();
 window.CHTCore = {};
 
 require('@cht-core/webapp/src/js/enketo/main.js');
+
+// Enable jQuery support for self-closing xml tags
+// https://jquery.com/upgrade-guide/3.5/
+const rxhtmlTag = /<(?!area|br|col|embed|hr|img|input|link|meta|param)(([a-z][^/\0>\x20\t\r\n\f]*)[^>]*)\/>/gi;
+// eslint-disable-next-line no-import-assign
+Object.defineProperties($, {
+  htmlPrefilter: { value: (html) => html.replace(rxhtmlTag, '<$1></$2>') }
+});
+
 const xpathExtension = require('@cht-core/webapp/src/js/enketo/medic-xpath-extensions');
 xpathExtension.init({}, toBik_text, moment);
