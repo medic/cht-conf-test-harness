64e41dfe4d4ad2fab7dec55579b165c855e9a5cc
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-03-26 14:05:00 -0500

    Remove window.$$

--- src/form-host/cht-environment.js
+++ src/form-host/cht-environment.js
@@ -1,8 +1,5 @@
 const { useFakeTimers } = require('sinon/lib/sinon/util/fake-timers');
 
-// // backward compatibility with harness v2
-window.$$ = window.$;
-
 let clock;
 window.fakeTimers = (...args) => {
   window.restoreTimers();
