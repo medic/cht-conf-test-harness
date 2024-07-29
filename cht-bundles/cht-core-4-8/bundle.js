3c78a723719ca9b39ed0bdbe263b779b3680d781
kennsippell <kennsippell@gmail.com>
2024-07-29 11:03:53 -0700

    Rebundle with contact-types-utils

--- cht-bundles/cht-core-4-8/bundle.js
+++ cht-bundles/cht-core-4-8/bundle.js
@@ -3,6 +3,7 @@ module.exports = {
   RegistrationUtils: require('../../build/cht-core-4-8/shared-libs/registration-utils'),
   CalendarInterval: require('../../build/cht-core-4-8/shared-libs/calendar-interval'),
   RulesEngineCore: require('../../build/cht-core-4-8/shared-libs/rules-engine'),
+  ContactTypesUtils: require('../../build/cht-core-4-8/shared-libs/contact-types-utils'),
   RulesEmitter: require('../../build/cht-core-4-8/shared-libs/rules-engine/src/rules-emitter'),
   nootils: require('../../build/cht-core-4-8/node_modules/cht-nootils'),
   Lineage: require('../../build/cht-core-4-8/shared-libs/lineage'),
